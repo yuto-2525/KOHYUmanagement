e89a64bde063468403f63b7fcbccc054288e1add
yuto-2525 <ytoz.25.05@gmail.com>
2025-07-03 23:43:33 +0900

    Update database.py for Render　

--- database.py
+++ database.py
@@ -2,10 +2,13 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os 
+
 
 db = SQLAlchemy()
 
 def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///events.db'
     db.init_app(app)
     with app.app_context():
         db.create_all()
